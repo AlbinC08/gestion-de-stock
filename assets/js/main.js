8acab60ab3a751496a5dbb89ec171919b01dd8ca
AlbinC08 <cedric.albin13780@gmail.com>
2022-06-19 20:14:33 +0200

    gestion de stock

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,10 @@ document.querySelector("#Produit_1").innerText = products[0] + " " + stock[0]
 document.querySelector("#Produit_2").innerText = products[1] + " " + stock[1]
 document.querySelector("#Produit_3").innerText = products[2] + " " + stock[2]
 
+for (let i = 0; i < document.querySelectorAll('.product').length; i++) {
+    document.querySelectorAll('.product')[i].innerText = products[i];
+}
+
 function cmd() {
     let result = 0
     let stock1Value = document.querySelector('#produit1').value
@@ -13,6 +17,9 @@ function cmd() {
 
     if (parseInt(stock1Value) > parseInt(stock[0]) || parseInt(stock2Value) > parseInt(stock[1]) || parseInt(stock3Value) > parseInt(stock[2])) {
         document.querySelector("#no_stock").innerText = 'Commande impossible, stock insufisant'
+        for (let i = 0; i < document.querySelectorAll('.produit').length; i++) {
+            document.querySelectorAll('.produit')[i].value = '';
+        }
     } else {
         result = subtract(stock[0], stock1Value)
         stock[0] = result
@@ -24,6 +31,9 @@ function cmd() {
         stock[2] = result
         document.querySelector("#Produit_3").innerText = products[2] + " " + stock[2]
         document.querySelector("#no_stock").innerText = ''
+        for (let i = 0; i < document.querySelectorAll('.produit').length; i++) {
+            document.querySelectorAll('.produit')[i].value = '';
+        }
     }
 }
 
@@ -32,3 +42,12 @@ function subtract(element, nmb) {
 }
 
 
+
+
+
+/****
+
+document.querySelector('#produit1').value=''
+document.querySelector('#produit2').value=''
+document.querySelector('#produit3').value=''
+*** */
